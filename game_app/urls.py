eeb716d920a214e057e669add8c1c06711cc8b8e
kachkynova-artur <kachkynov_a@iuca.kg>
2025-10-26 16:50:28 +0600

    Add urls for players, results, and game

---
+++ game_app/urls.py
@@ -0,0 +1,19 @@
+# game_app/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # Игроки
+    path('players/', views.player_list, name='player_list'),
+    path('players/create/', views.player_create, name='player_create'),
+    path('players/<int:pk>/edit/', views.player_update, name='player_update'),
+    path('players/<int:pk>/delete/', views.player_delete, name='player_delete'),
+
+    # Результаты
+    path('results/', views.result_list, name='result_list'),
+    path('results/<int:pk>/edit/', views.result_update, name='result_update'),
+    path('results/<int:pk>/delete/', views.result_delete, name='result_delete'),
+
+    # Игра
+    path('players/<int:player_id>/play/', views.play_game, name='play_game'),
+]
