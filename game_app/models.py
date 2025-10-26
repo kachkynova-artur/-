2a55c0492b5c0de5494ad4eca3c128cb4b8ceed0
kachkynova-artur <kachkynov_a@iuca.kg>
2025-10-26 16:42:59 +0600

    Add models: Player and GameResult

--- game_app/models.py
+++ game_app/models.py
@@ -1,3 +1,18 @@
+
 from django.db import models
 
-# Create your models here.
+class Player(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class GameResult(models.Model):
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    number_to_guess = models.IntegerField()
+    attempts = models.IntegerField()
+    success = models.BooleanField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.player.name} - {'Win' if self.success else 'Lose'} - {self.date}"
